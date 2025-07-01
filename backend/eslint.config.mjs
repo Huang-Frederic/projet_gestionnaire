751867b18b2f4c28de7aad220a1a6a663204ed5f
Huang Frederic <huangfrederic2002@gmail.com>
2025-07-01 15:35:14 +0200

    updating eslint.config.mjs

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,18 +2,24 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig({
-  parserOptions: {
-    ecmaVersion: 2025, 
-    sourceType: "module"
+  languageOptions: {
+    parserOptions: {
+      ecmaVersion: 2020,  
+      sourceType: "module",
+    },
   },
   overrides: [
     {
       files: ["**/*.js"],
-      languageOptions: { sourceType: "module" }
+      languageOptions: {
+        sourceType: "module",  
+      },
     },
     {
       files: ["**/*.{js,mjs,cjs}"],
-      languageOptions: { globals: globals.browser }
-    }
-  ]
+      languageOptions: {
+        globals: globals.browser,
+      },
+    },
+  ],
 });
