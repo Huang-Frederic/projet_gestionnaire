9e3b8c487db4f48cf7ee176041291c776965bb57
Huang Frederic <huangfrederic2002@gmail.com>
2025-07-01 16:05:44 +0200

    working test for eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,24 @@
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-  languageOptions: {
-    parserOptions: {
-      ecmaVersion: 2020,  
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
       sourceType: "module",
     },
   },
-  overrides: [
-    {
-      files: ["**/*.js"],
-      languageOptions: {
-        sourceType: "module",  
-      },
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
     },
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-      languageOptions: {
-        globals: globals.browser,
-      },
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
     },
-  ],
-});
+  },
+];
