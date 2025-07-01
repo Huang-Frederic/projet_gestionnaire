847b7e4882897f89b1f5d58d437d768cbb69af5d
Huang Frederic <huangfrederic2002@gmail.com>
2025-07-01 16:15:46 +0200

    eslint working now

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,24 +1,33 @@
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 import globals from "globals";
 
 export default [
   {
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-  },
-
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "module",
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
     },
-  },
-
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
-      globals: globals.browser,
+    rules: {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
